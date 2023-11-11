09b3275b824c85f1ac13ca9e9df7fcc66544d97b
Pierce Governale <piercegovernale@gmail.com>
2023-11-10 17:06:56 -0800

    Add Color Switching and Reset

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 use std::time::{SystemTime, UNIX_EPOCH};
 
-use bevy::{prelude::*, window::{WindowResized, WindowResolution}, core_pipeline::{tonemapping::Tonemapping, bloom::BloomSettings}};
+use bevy::{prelude::*, window::{WindowResized, WindowResolution}, core_pipeline::{tonemapping::Tonemapping, bloom::BloomSettings}, render::color};
 use bevy_hanabi::prelude::*;
 use libnoise::prelude::*;
 
@@ -32,14 +32,68 @@ fn main() {
             width: WIDTH as f32,
             height: HEIGHT as f32
         })
-        .insert_resource(NoiseGen { gen: Source::perlin(seed), seed, noise_scale: 0.005 })
+        .insert_resource(NoiseGen { gen: Source::perlin(seed), seed, noise_scale: 0.001 })
+        .insert_resource(ParticleCount(NUM_PARTICLES as usize))
+        .insert_resource(ColorScheme {
+            background: Color::BLACK,
+            particle: Color::WHITE,
+            luminosity: LUMINOSITY
+        })
+        // .insert_resource(Luminosity(LUMINOSITY))
         .add_systems(Startup, (setup_camera, add_particles))
         .add_systems(PostStartup, draw_trails)
-        .add_systems(Update, (move_particles, keyboard_input)) // check_field, 
+        .add_systems(Update, (move_particles, keyboard_input, ensure_particle_count)) // check_field, 
         .add_systems(Update, window_resize)
         .run();
 }
 
+#[derive(Resource)]
+struct Luminosity(f32);
+
+#[derive(Resource)]
+struct ParticleCount(usize);
+
+#[derive(Resource, Copy, Clone)]
+struct ColorScheme {
+    background: Color,
+    particle: Color,
+    luminosity: f32,
+}
+
+impl ColorScheme {
+    fn default() -> Self {
+        ColorScheme {
+            background: Color::BLACK,
+            particle: Color::WHITE,
+            luminosity: LUMINOSITY
+        }
+    }
+
+    fn custom(background: Color, particle: Color, luminosity: f32) -> Self {
+        ColorScheme {
+            background,
+            particle,
+            luminosity
+        }
+    }
+
+    fn random() -> Self {
+        ColorScheme {
+            background: Color::rgb(rand::random::<f32>(), rand::random::<f32>(), rand::random::<f32>()),
+            particle: Color::rgb(rand::random::<f32>(), rand::random::<f32>(), rand::random::<f32>()),
+            luminosity: LUMINOSITY
+        }
+    }
+
+    fn inverse(&self) -> Self {
+        ColorScheme {
+            background: self.particle,
+            particle: self.background,
+            luminosity: self.luminosity
+        }
+    }
+
+}
 
 #[derive(Component, Copy, Clone)]
 struct Particle {
@@ -102,12 +156,26 @@ fn window_resize(mut resize_reader: EventReader<WindowResized>,
     }
 }
 
-fn add_particles(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<ColorMaterial>>, window_size: Res<WindowSize>) {
-    for _ in 0..NUM_PARTICLES as usize {
+fn add_particles(mut commands: Commands, window_size: Res<WindowSize>, particle_count: Res<ParticleCount>) {
+    for _ in 0..particle_count.0 as usize {
             // add a particle
             let particle: Particle = Particle::random(window_size.width, window_size.height);
 
             commands.spawn(particle);
+    }
+}
+
+fn ensure_particle_count(mut commands: Commands, particle_count: Res<ParticleCount>, mut particles: Query<Entity, With<Particle>>) {
+    if particles.iter().count() < particle_count.0 {
+        // add a particle
+        let particle: Particle = Particle::random(WIDTH as f32, HEIGHT as f32);
+
+        commands.spawn(particle);
+    } else if particles.iter().count() > particle_count.0 {
+        // remove a particle
+        for entity in particles.iter().take(particles.iter().count() - particle_count.0) {
+            commands.entity(entity).despawn_recursive();
+        }
     }
 }
 
@@ -130,7 +198,18 @@ fn move_particles(mut particles: Query<(&mut Particle, &mut Transform)>, window_
     }
 }
 
-fn keyboard_input(keys: Res<Input<KeyCode>>, mut noise_gen: ResMut<NoiseGen>) {
+fn keyboard_input(mut commands: Commands, 
+                  keys: Res<Input<KeyCode>>, 
+                  window_size: Res<WindowSize>,
+                  mut noise_gen: ResMut<NoiseGen>, 
+                  mut color_scheme: ResMut<ColorScheme>,
+                  mut effects: ResMut<Assets<EffectAsset>>,
+                  mut particles: Query<&mut Particle>,
+                  mut particle_entities: Query<Entity, With<Particle>>) {
+    
+    let mut should_change_particle_effects: bool = false;
+    let mut new_color_scheme: ColorScheme = color_scheme.clone();
+    
     if keys.just_pressed(KeyCode::Space) {
         noise_gen.seed = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs() as u64;
         noise_gen.gen = Source::perlin(noise_gen.seed);
@@ -141,9 +220,64 @@ fn keyboard_input(keys: Res<Input<KeyCode>>, mut noise_gen: ResMut<NoiseGen>) {
     } else if keys.just_pressed(KeyCode::Down) {
         noise_gen.noise_scale *= 2.;
     }
+
+    if keys.just_pressed(KeyCode::R) {
+        // reset the particle positions and clear the effects
+        should_change_particle_effects = true;
+        for mut particle in particles.iter_mut() {
+            particle.x = -window_size.width / 2. + rand::random::<f32>() * window_size.width;
+            particle.y = -window_size.height / 2. + rand::random::<f32>() * window_size.height;
+        }
+    }
+
+    if keys.just_pressed(KeyCode::I) {
+        // invert the color scheme
+        should_change_particle_effects = true;
+        *color_scheme = color_scheme.inverse();
+    }
+
+    if keys.just_pressed(KeyCode::N) {
+        // new random color scheme
+        should_change_particle_effects = true;
+        new_color_scheme = ColorScheme::random();
+    }
+
+
+    // keep these last, they modify the new color
+    if keys.just_pressed(KeyCode::A) {
+        // decrease the luminosity
+        should_change_particle_effects = true;
+        new_color_scheme.luminosity -= 1.;
+        println!("Luminosity: {}", new_color_scheme.luminosity);
+
+    } else if (keys.just_pressed(KeyCode::D)) {
+        // increase the luminosity
+        should_change_particle_effects = true;
+        new_color_scheme.luminosity += 1.;
+        println!("Luminosity: {}", new_color_scheme.luminosity);
+    }
+
+    if should_change_particle_effects {
+        // commands.insert_resource(ClearColor(new_color_scheme.background.clone()));
+        change_particle_effects(commands, effects, particle_entities, new_color_scheme);
+        color_scheme.particle = new_color_scheme.particle;
+        color_scheme.background = new_color_scheme.background;
+        color_scheme.luminosity = new_color_scheme.luminosity;
+    }
+
 }
 
-fn draw_trails(mut commands: Commands, mut effects: ResMut<Assets<EffectAsset>>, particles: Query<Entity, With<Particle>>) {
+fn change_particle_effects(mut commands: Commands, mut effects: ResMut<Assets<EffectAsset>>, particles: Query<Entity, With<Particle>>, color_scheme: ColorScheme) {
+    for entity in particles.iter() {
+        commands.entity(entity).remove::<ParticleEffectBundle>();
+    }
+
+    add_particle_effects(commands, effects, particles, color_scheme);
+}
+
+fn add_particle_effects(mut commands: Commands, mut effects: ResMut<Assets<EffectAsset>>, particles: Query<Entity, With<Particle>>, color_scheme: ColorScheme) {
+    commands.insert_resource(ClearColor(color_scheme.background));
+    
     for entity in particles.iter() {
 
         let writer = ExprWriter::new();
@@ -152,11 +286,15 @@ fn draw_trails(mut commands: Commands, mut effects: ResMut<Assets<EffectAsset>>,
 
         let lifetime = writer.lit(1.).expr();
         let init_lifetime = SetAttributeModifier::new(Attribute::LIFETIME, lifetime);
+
+        let particle_color: Color = color_scheme.particle;
+
+        let luminosity: f32 = color_scheme.luminosity;
         
         let mut gradient = Gradient::new();
-        gradient.add_key(0.0, Vec4::new(LUMINOSITY, LUMINOSITY, LUMINOSITY, 0.2)); // Start color: white, fully opaque
-        gradient.add_key(0.5, Vec4::new(LUMINOSITY, LUMINOSITY, LUMINOSITY, 0.05));
-        gradient.add_key(1., Vec4::new(LUMINOSITY, LUMINOSITY, LUMINOSITY, 0.0)); // End color: white, fully transparent
+        gradient.add_key(0.0, Vec4::new(particle_color.r() * luminosity, particle_color.g() * luminosity, particle_color.b() * luminosity, 0.2));
+        gradient.add_key(0.5, Vec4::new(particle_color.r() * luminosity, particle_color.g() * luminosity, particle_color.b() * luminosity, 0.05));
+        gradient.add_key(1.0, Vec4::new(particle_color.r() * luminosity, particle_color.g() * luminosity, particle_color.b() * luminosity, 0.0));
 
         let init_pos = 
             SetPositionCircleModifier {
@@ -174,7 +312,7 @@ fn draw_trails(mut commands: Commands, mut effects: ResMut<Assets<EffectAsset>>,
             };
         
         let effect = effects.add(
-            EffectAsset::new(1024, Spawner::rate(60.0.into()), writer.finish())
+            EffectAsset::new(1024, Spawner::rate(45.0.into()), writer.finish())
             .with_name("trail")
             .init(init_pos)
             .init(init_vel)
@@ -192,4 +330,8 @@ fn draw_trails(mut commands: Commands, mut effects: ResMut<Assets<EffectAsset>>,
             ..default()
         });
     }
+}
+
+fn draw_trails(mut commands: Commands, mut effects: ResMut<Assets<EffectAsset>>, particles: Query<Entity, With<Particle>>, color_scheme: Res<ColorScheme>) {
+    add_particle_effects(commands, effects, particles, color_scheme.clone());
 }
